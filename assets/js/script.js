dc0b2e70b2df5eccabe1f85d3545b70075198586
gabrielavilchesp@gmail.com <gabrielavilches@MBP-de-Gabriela.deafiolatam.com>
2019-05-15 18:48:51 -0400

    carrousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 $(function () {
-  $('[data-toggle="popover"]').popover()
+	$('[data-toggle="popover"]').popover()
 })
+	$("a").click(function(event){
+		if (this.hash !== "") {
+			event.preventdefault();
+			var gato = this.hash;
+			$("html,body").animate({
+				scrolltop: $(gato).offset().top
+			},800,function(){
+				window.location.has = gato;
+			});
+
+		}
+	});
