263bce3d00747d277a4e14aeb795575bcb7649bc
gabrielavilchesp@gmail.com <gabrielavilches@MBP-de-Gabriela.deafiolatam.com>
2019-05-15 17:22:24 -0400

    responsive hide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-$(function(){
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
-}
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
